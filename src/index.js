1c5f94c898048953b776896f3c6d6cefbac43659
pablos <ppapad_33@yahoo.gr>
2018-07-09 18:02:13 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('./scss/main.scss');
+import homeIcon from './img/logo.png';
+
+// var homeImg = document.getElementById('home');
+// homeImg.src = homeIcon;
+
+// console.log('hello world');
+// console.log("Hello, world 2");
+// if(module.hot) {
+//     module.hot.accept();
+// }
